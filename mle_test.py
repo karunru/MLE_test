8b8a49a94ac12bae5d02e680d75553a454fede17
karunru <1qazhikaru@gmail.com>
2015-12-08 13:14:33 +0900

    最大値に点線を追加

--- mle_test.py
+++ mle_test.py
@@ -16,6 +16,12 @@ theta = np.arange(0,1,0.01)
 P1 = np.power(theta, r1) * np.power(1-theta, n-r1)
 P2 = np.power(theta, r2) * np.power(1-theta, n-r2)
 
+print(r1/n)
+print(r2/n)
+
 plt.plot(theta, P1, "b")
+plt.plot([r1/n,r1/n],[0,P1[r1/n/0.01]], "b:")
 plt.plot(theta, P2, "r")
+plt.plot([r2/n,r2/n],[0,P2[r2/n/0.01]], "r:")
 plt.show()
+
